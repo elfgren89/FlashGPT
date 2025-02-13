19ba88564aa38b0c344b370932156bfde8e3da5e
elfgren89 <elfgren89@gmail.com>
2025-02-13 20:41:28 +0100

    Render ts

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,20 @@
 module.exports = {
-    apps: [
-      {
-        name: "backend",
-        script: "/app/backend/server.js",
-        env: {
-          NODE_ENV: "production"
-        },
-        env_file: "/app/backend/.env"
+  apps: [
+    {
+      name: "backend",
+      script: "/app/backend/server.js",
+      env: {
+        NODE_ENV: "production"
       },
-      {
-        name: "frontend",
-        script: "npx",
-        args: "serve -s /app/frontend/build -l 3000",
-        env: {
-          NODE_ENV: "production"
-        }
+      env_file: "/app/backend/.env"
+    },
+    {
+      name: "frontend",
+      script: "serve",
+      args: "-s /app/frontend/build -l 0.0.0.0 -p 3000",
+      env: {
+        NODE_ENV: "production"
       }
-    ]
-  };
-  
+    }
+  ]
+};
